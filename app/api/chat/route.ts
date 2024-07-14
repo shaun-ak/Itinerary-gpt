bf2d2411d5eea6683cf22dd4eecde1f637a78d60
Shaunak <shaundj20@gmail.com>
2024-07-14 08:46:24 +0530

    fix(TSA-1): fix vulnerabilities for local

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -10,7 +10,7 @@ export const runtime = "edge";
 
 export async function POST(req: NextRequest) {
   const { messages } = await req.json();
-  const prompt = `You are an AI Itinerary planning specialist who specializes in giving perfect itinerary for holiday destinations, \
+  const prompt = `You are an AI Itinerary planning specialist named 'Itinerary-GPT' who specializes in giving perfect itinerary for holiday destinations, \
     give a detailed itinerary in the form of days like: day 1: ---- , day 2: ----, etc. Get the destination \
     from UserInput and be creative with your answers. Also, if user asks normal questions other than itinerary related questions\
     then be clever and answer in a user friendly way.
