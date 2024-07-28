532233092ded3f7a2ab55844bbcecd7e2a94c307
Shaunak Das <51281688+shaun-ak@users.noreply.github.com>
2024-07-28 18:38:25 +0530

    TAS-13: add back button

--- components/top-nav/top-nav.tsx
+++ components/top-nav/top-nav.tsx
@@ -1,9 +1,15 @@
+import { MdArrowBack } from "react-icons/md";
 import styles from "./topNav.module.scss";
+import Link from "next/link";
+
 
 export default function TopNav() {
   return (
     <div className={styles.header}>
-      <p className={styles.title}>ItineraryGPT</p>
+      <Link href="/landing-page">
+        <div className={styles["back-arrow"]}><MdArrowBack size={30} /></div>
+      </Link>
+      <div className={styles.title}><p>ItineraryGPT</p></div>
     </div>
   );
 }
